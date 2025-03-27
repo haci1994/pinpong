d8ebe6256a9c7a2ff488c82eeb7fa6abcadb992c
haci1994 <mmzadj@gmail.com>
2025-03-27 09:49:45 +0400

    final game

--- Pinkpong Game/Form1.Designer.cs
+++ Pinkpong Game/Form1.Designer.cs
@@ -33,6 +33,13 @@
             timer1 = new System.Windows.Forms.Timer(components);
             panelKick = new Panel();
             btnReset = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            textBoxColumn = new TextBox();
+            label1 = new Label();
+            textboxRow = new TextBox();
+            btnPause = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // btnBall
@@ -44,7 +51,7 @@
             btnBall.FlatAppearance.MouseDownBackColor = Color.RoyalBlue;
             btnBall.FlatAppearance.MouseOverBackColor = Color.DodgerBlue;
             btnBall.FlatStyle = FlatStyle.Flat;
-            btnBall.Location = new Point(471, 283);
+            btnBall.Location = new Point(491, 516);
             btnBall.Name = "btnBall";
             btnBall.Size = new Size(15, 15);
             btnBall.TabIndex = 0;
@@ -66,13 +73,79 @@
             // 
             // btnReset
             // 
-            btnReset.Location = new Point(16, 12);
+            btnReset.Location = new Point(63, 79);
             btnReset.Name = "btnReset";
-            btnReset.Size = new Size(75, 23);
+            btnReset.Size = new Size(100, 23);
             btnReset.TabIndex = 2;
-            btnReset.Text = "Play Again";
+            btnReset.Text = "Play";
             btnReset.UseVisualStyleBackColor = true;
             btnReset.Click += btnReset_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(128, 64, 64);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBoxColumn);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(btnReset);
+            panel1.Controls.Add(textboxRow);
+            panel1.Location = new Point(386, 257);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(232, 129);
+            panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
+            label2.ForeColor = Color.Snow;
+            label2.Location = new Point(130, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Column Size";
+            label2.Click += label2_Click;
+            // 
+            // textBoxColumn
+            // 
+            textBoxColumn.Location = new Point(126, 40);
+            textBoxColumn.Name = "textBoxColumn";
+            textBoxColumn.Size = new Size(88, 23);
+            textBoxColumn.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
+            label1.ForeColor = Color.Snow;
+            label1.Location = new Point(31, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Row Size";
+            // 
+            // textboxRow
+            // 
+            textboxRow.Location = new Point(14, 40);
+            textboxRow.Name = "textboxRow";
+            textboxRow.Size = new Size(88, 23);
+            textboxRow.TabIndex = 0;
+            // 
+            // btnPause
+            // 
+            btnPause.BackColor = Color.DarkKhaki;
+            btnPause.BackgroundImageLayout = ImageLayout.None;
+            btnPause.Cursor = Cursors.Hand;
+            btnPause.FlatStyle = FlatStyle.Flat;
+            btnPause.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic);
+            btnPause.ForeColor = Color.DarkOliveGreen;
+            btnPause.Location = new Point(902, 5);
+            btnPause.Name = "btnPause";
+            btnPause.Size = new Size(75, 23);
+            btnPause.TabIndex = 4;
+            btnPause.Text = "Pause";
+            btnPause.UseVisualStyleBackColor = false;
+            btnPause.Click += button1_Click;
             // 
             // Form1
             // 
@@ -80,15 +153,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Moccasin;
             ClientSize = new Size(984, 561);
-            Controls.Add(btnReset);
-            Controls.Add(panelKick);
+            Controls.Add(btnPause);
+            Controls.Add(panel1);
             Controls.Add(btnBall);
+            Controls.Add(panelKick);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             MaximumSize = new Size(1000, 600);
             MinimumSize = new Size(1000, 600);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "PingPong";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -98,5 +174,11 @@
         private System.Windows.Forms.Timer timer1;
         private Panel panelKick;
         private Button btnReset;
+        private Panel panel1;
+        private Label label1;
+        private TextBox textboxRow;
+        private Button btnPause;
+        private Label label2;
+        private TextBox textBoxColumn;
     }
 }
--- Pinkpong Game/Form1.cs
+++ Pinkpong Game/Form1.cs
@@ -9,14 +9,20 @@ namespace Pinkpong_Game
         private int _xSpeed = 2;
         private int _ySpeed = 2;
         private bool _gameOver = false;
+        private bool _gamePause = false;
+        private bool _won = false;
         private List<Panel> _panels;
+        private List<Panel> _shadowPanels;
+        private int _rowSize = 3;
+        private int _columnSize = 4;
 
         public Form1()
         {
-            InitializeComponent();
-            _panels = MakeButtons(8, 5);
 
-            timer1.Start();
+            InitializeComponent();
+            //_panels = MakeButtons(_columnSize, _rowSize);
+            btnPause.Hide();
+            //timer1.Start();
         }
 
         public void CheckPanelTouch()
@@ -26,38 +32,41 @@ namespace Pinkpong_Game
             int br = btnBall.Right;
             int bl = btnBall.Left;
 
-            
-            for (int i=0; i< _panels.Count; i++)
+
+            for (int i = 0; i < _panels.Count; i++)
             {
                 var panel = _panels[i];
 
-                if (bt <= panel.Bottom && br >= panel.Left && bl <= panel.Right)
+                //panelin alti
+                if (bt >= panel.Bottom - 2 && bt <= panel.Bottom + 2 && br >= panel.Left && bl <= panel.Right)
                 {
                     Controls.Remove(panel);
                     _panels.Remove(panel);
-                    _ySpeed = - _ySpeed;
+                    _ySpeed = -_ySpeed;
+                }
+                //panelin ustu
+                if (bb >= panel.Top - 2 && bb <= panel.Top + 2 && br >= panel.Left && bl <= panel.Right)
+                {
+                    Controls.Remove(panel);
+                    _panels.Remove(panel);
+                    _ySpeed = -_ySpeed;
                 }
 
-                //if (bt <= panel.Bottom || bb >= panel.Top && br >= panel.Left)
-                //{
-                //    Controls.Remove(panel);
-                //    _panels.Remove(panel);
-                //    _ySpeed = -_ySpeed;
-                //}
+                //panelin solu
+                if (br >= panel.Left - 2 && br <= panel.Left + 2 && ((bt <= panel.Bottom && bt >= panel.Top) || (bb >= panel.Top && bb <= panel.Bottom)))
+                {
+                    Controls.Remove(panel);
+                    _panels.Remove(panel);
+                    _xSpeed = -_xSpeed;
+                }
 
-                //if (bt <= panel.Bottom || bb >= panel.Top && bl <= panel.Right)
-                //{
-                //    Controls.Remove(panel);
-                //    _panels.Remove(panel);
-                //    _ySpeed = -_ySpeed;
-                //}
-
-                //if (bb >= panel.Top && br >= panel.Left && bl <= panel.Right)
-                //{
-                //    Controls.Remove(panel);
-                //    _panels.Remove(panel);
-                //    _ySpeed = -_ySpeed;
-                //}
+                //panelin sagi
+                if (bl >= panel.Right - 2 && bl <= panel.Right + 2 && ((bt <= panel.Bottom && bt >= panel.Top) || (bb >= panel.Top && bb <= panel.Bottom)))
+                {
+                    Controls.Remove(panel);
+                    _panels.Remove(panel);
+                    _xSpeed = -_xSpeed;
+                }
             }
         }
 
@@ -66,7 +75,35 @@ namespace Pinkpong_Game
             if (_gameOver)
             {
                 timer1.Stop();
+                panel1.Show();
+                btnPause.Hide();
+                foreach (var panel in _panels)
+                {
+                    Controls.Remove(panel);
+                }
+                foreach (var panel in _shadowPanels)
+                {
+                    Controls.Remove(panel);
+                }
+                _panels.Clear();
                 MessageBox.Show("Game OVER!");
+            }
+
+            if (_won)
+            {
+                timer1.Stop();
+                panel1.Show();
+                btnPause.Hide();
+                foreach (var panel in _panels)
+                {
+                    Controls.Remove(panel);
+                }
+                foreach (var panel in _shadowPanels)
+                {
+                    Controls.Remove(panel);
+                }
+                _panels.Clear();
+                MessageBox.Show("CLEAR! GOOD JOB!");
             }
         }
 
@@ -75,9 +112,9 @@ namespace Pinkpong_Game
 
             if (w > 10 || h > 6)
             {
-                MessageBox.Show("You exceeded limits...");
-                _gameOver = true;
-                return default;
+                MessageBox.Show("You exceeded limits... Max size is 6x10");
+                w = 10;
+                h = 6;
             }
             int btnW = 70;
             int btwn = 5;
@@ -105,10 +142,46 @@ namespace Pinkpong_Game
             return panelss;
         }
 
+        public List<Panel> MakeShadowButtons(int w, int h)
+        {
+
+            if (w > 10 || h > 6)
+            {
+                w = 10;
+                h = 6;
+            }
+            int btnW = 70;
+            int btwn = 5;
+            int leftSpare = (ClientSize.Width - btnW * w - 5 * (w - 1)) / 2;
+
+            List<Panel> panelss = new List<Panel>();
+
+            for (int i = 0; i < h; i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    var panel = new Panel();
+
+                    panel.Width = btnW;
+                    panel.Height = 20;
+                    panel.Left = leftSpare + j * (btnW + btwn);
+                    panel.Top = 60 + i * 25;
+                    panel.BackColor = Color.White;
+
+                    panelss.Add(panel);
+                    Controls.Add(panel);
+                }
+            }
+
+            return panelss;
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
 
             CheckGameOver();
+
+            if (_panels.Count == 0) _won = true;
 
             btnBall.Left += _xSpeed;
             btnBall.Top += _ySpeed;
@@ -147,7 +220,7 @@ namespace Pinkpong_Game
                 if (_xSpeed < 0)
                 {
                     _ySpeed = -_ySpeed;
-                    _xSpeed = _xSpeed- 1;
+                    _xSpeed = -3;
                 }
             }
 
@@ -173,7 +246,7 @@ namespace Pinkpong_Game
                 if (_xSpeed > 0)
                 {
                     _ySpeed = -_ySpeed;
-                    _xSpeed += 1;
+                    _xSpeed = 3;
                 }
 
                 if (_xSpeed < 0)
@@ -209,7 +282,49 @@ namespace Pinkpong_Game
             _ySpeed = 2;
 
             _gameOver = false;
+            _won = false;
+
+            try
+            {
+                _panels = MakeButtons(int.Parse(textboxRow.Text), int.Parse(textBoxColumn.Text));
+                _shadowPanels = MakeShadowButtons(int.Parse(textboxRow.Text), int.Parse(textBoxColumn.Text));
+            }
+            catch
+            {
+                MessageBox.Show("You did not enter row and column size! Default size will be 5 x 5");
+                _panels = MakeButtons(5, 5);
+                _shadowPanels = MakeShadowButtons(5, 5);
+            }
+
             timer1.Start();
+            panel1.Hide();
+            btnPause.Show();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_gamePause)
+            {
+                timer1.Start();
+                btnPause.Text = "Pause";
+                _gamePause = false;
+            }
+            else if (!_gamePause)
+            {
+                timer1.Stop();
+                btnPause.Text = "Continue";
+                _gamePause = true;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
